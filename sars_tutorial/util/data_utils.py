6e62973b2fe0d4c96a7761785baa6e5207f17050
David Przybilla <dav.alejandro@gmail.com>
2020-11-09 21:05:44 +0900

    formatting

--- sars_tutorial/util/data_utils.py
+++ sars_tutorial/util/data_utils.py
@@ -6,31 +6,30 @@ from collections import Counter
 
 import numpy as np
 import pandas as pd
-from spotlight.interactions import SequenceInteractions
 
 
-def pad(seq, max_seq_len, pre=False):	
-    initial_len = len(seq)	
-    if max_seq_len < initial_len:	
-        return np.array(seq[:max_seq_len], dtype="int32")	
-    to_fill = max_seq_len - initial_len	
-    array_to_fill = np.zeros(to_fill, dtype="int32")	
-    data_tup = (array_to_fill, seq) if pre is True else (seq, array_to_fill)	
+def pad(seq, max_seq_len, pre=False):
+    initial_len = len(seq)
+    if max_seq_len < initial_len:
+        return np.array(seq[:max_seq_len], dtype="int32")
+    to_fill = max_seq_len - initial_len
+    array_to_fill = np.zeros(to_fill, dtype="int32")
+    data_tup = (array_to_fill, seq) if pre is True else (seq, array_to_fill)
     return np.concatenate(data_tup).astype("int32")
 
 
-def display_seq_statistics(dataset):	
-    ds_seq_len = dataset["sequence"].apply(lambda x: len(x))	
-    min_seq = ds_seq_len.min()	
-    max_seq = ds_seq_len.max()	
-    std_seq = ds_seq_len.std()	
-    mean_seq = ds_seq_len.mean()	
-    cnt = Counter()	
-    for seq_len in ds_seq_len.values:	
-        cnt[seq_len] += 1	
-    sorted_cnt = sorted(cnt.items(), key=lambda x: x[0])	
-    for k, v in sorted_cnt:	
-        print("Seq len: {} -> count: {}".format(k, v))	
+def display_seq_statistics(dataset):
+    ds_seq_len = dataset["sequence"].apply(lambda x: len(x))
+    min_seq = ds_seq_len.min()
+    max_seq = ds_seq_len.max()
+    std_seq = ds_seq_len.std()
+    mean_seq = ds_seq_len.mean()
+    cnt = Counter()
+    for seq_len in ds_seq_len.values:
+        cnt[seq_len] += 1
+    sorted_cnt = sorted(cnt.items(), key=lambda x: x[0])
+    for k, v in sorted_cnt:
+        print("Seq len: {} -> count: {}".format(k, v))
     print("Sequence length min: {} , max: {}, mean: {}, std: {}".format(min_seq, max_seq, mean_seq, std_seq))
 
 
@@ -60,7 +59,6 @@ def dataset_to_gru4rec_format(dataset):
     """
     Convert a list of sequences to GRU4Rec format.
     Based on this StackOverflow answer: https://stackoverflow.com/a/48532692
-
     :param dataset: the dataset to be transformed
     """
 
@@ -73,16 +71,6 @@ def dataset_to_gru4rec_format(dataset):
     unstacked['ts'] = unstacked['ts'] + unstacked.groupby('user_id').cumcount()
     unstacked.rename(columns={'sequence': 'item_id'}, inplace=True)
     return unstacked
-
-
-def sequences_to_spotlight_format(sequences, max_seq_len=80):	
-    """	
-    Convert a list of sequences to Pytorch Spotlight format's	
-    :param dataset:	
-    :return:	
-    """	
-    padded = sequences["sequence"].apply(lambda x: pad(x, max_seq_len))	
-    return SequenceInteractions(np.array(list(padded.to_numpy())))
 
 
 def sequences_to_spfm_format(sequences, tmp_path='tmp/sequences.txt'):
@@ -129,37 +117,37 @@ def load_and_adapt(path, last_months=0):
     return data
 
 
-def get_test_sequences(test_data, given_k):	
-    # we can run evaluation only over sequences longer than abs(LAST_K)	
-    test_sequences = test_data.loc[test_data['sequence'].map(len) > abs(given_k), 'sequence'].values	
-    return test_sequences	
+def get_test_sequences(test_data, given_k):
+    # we can run evaluation only over sequences longer than abs(LAST_K)
+    test_sequences = test_data.loc[test_data['sequence'].map(len) > abs(given_k), 'sequence'].values
+    return test_sequences
 
 
-def get_test_sequences_and_users(test_data, given_k, train_users):	
-    # we can run evaluation only over sequences longer than abs(LAST_K)	
-    mask = test_data['sequence'].map(len) > abs(given_k)	
-    mask &= test_data['user_id'].isin(train_users)	
-    test_sequences = test_data.loc[mask, 'sequence'].values	
-    test_users = test_data.loc[mask, 'user_id'].values	
-    return test_sequences, test_users	
+def get_test_sequences_and_users(test_data, given_k, train_users):
+    # we can run evaluation only over sequences longer than abs(LAST_K)
+    mask = test_data['sequence'].map(len) > abs(given_k)
+    mask &= test_data['user_id'].isin(train_users)
+    test_sequences = test_data.loc[mask, 'sequence'].values
+    test_users = test_data.loc[mask, 'user_id'].values
+    return test_sequences, test_users
 
 
-def print_metrics(metrics, eval_results, rec_name, given_k, look_ahead, step, topn):	
-    print('Sequential evaluation for {} (GIVEN_K={}, LOOK_AHEAD={}, STEP={})'.format(rec_name, given_k, look_ahead, step, topn))	
-    for mname, mvalue in zip(metrics.keys(), eval_results):	
-        print('\t{}@{}: {:.4f}'.format(mname, topn, mvalue))	
+def print_metrics(metrics, eval_results, rec_name, given_k, look_ahead, step, topn):
+    print('Sequential evaluation for {} (GIVEN_K={}, LOOK_AHEAD={}, STEP={}, TOPN={})'.format(rec_name, given_k, look_ahead, step, topn))
+    for mname, mvalue in zip(metrics.keys(), eval_results):
+        print('\t{}@{}: {:.4f}'.format(mname, topn, mvalue))
 
 
-class WithMetadata:	
+class WithMetadata:
 
-    def load_metadata(self, path):	
-        """	
-        loads a jsonl file. the structure of the file has to be as follows:	
-        - {'itemid': xxx, 'properties': [ {'property': 'xx', 'value':'xx', 'ts': xxx} ] }	
-        """	
-        metadata = {}	
-        with open(path, 'r') as f:	
-            for line in f:	
-                data = json.loads(line)	
-                metadata[data['itemid']] = data	
+    def load_metadata(self, path):
+        """
+        loads a jsonl file. the structure of the file has to be as follows:
+        - {'itemid': xxx, 'properties': [ {'property': 'xx', 'value':'xx', 'ts': xxx} ] }
+        """
+        metadata = {}
+        with open(path, 'r') as f:
+            for line in f:
+                data = json.loads(line)
+                metadata[data['itemid']] = data
         return metadata
